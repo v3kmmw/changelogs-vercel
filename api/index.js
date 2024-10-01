7c0c50828e04d6bcb2dc2b78aa034ae410d089fb
v3kmmw <v3kmww@gmail.com>
2024-10-01 03:39:47 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000; // Set the port
-
+const fs = require('fs');
 // Serve your static HTML, CSS, and JS files
 app.use(express.static(path.join(__dirname, '../')));
 
