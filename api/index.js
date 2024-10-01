83c2a9c636e3decb265f65ea04392a84f58518d4
v3kmmw <v3kmww@gmail.com>
2024-10-01 04:23:25 -0500

    g

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 5500; // Set the port
